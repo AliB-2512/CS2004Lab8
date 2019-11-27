3f8da8ec1a4efb541709e4e1b95cae7d9ba1ef4f
Ali Bachar <alibachar@icloud.com>
2019-11-27 19:07:04 +0000

    8.5 Exercise 4

---
+++ src/Lab8.java
@@ -0,0 +1,10 @@
+public class Lab8 {
+	public static void main(String args[])
+	{
+		ScalesSolution s = new ScalesSolution("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		s.println();
+		
+		
+	}
+	
+}
